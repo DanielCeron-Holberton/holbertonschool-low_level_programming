520859877328f5d932374ac7eea16649bc2b72b9
DanielCeron-Holberton <3403@holbertonschool.com>
2021-06-17 23:50:25 +0000

    [ADD] - 6th proyect added

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,23 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int integerType;
+float floatType;
+long int longIntegerType;
+long long int LonglongIntegerType;
+char charType;
+    
+printf("Size of char: %ld byte\n",sizeof(charType));
+printf("Size of int: %ld bytes\n",sizeof(integerType));
+printf("Size of long int: %ld bytes\n",sizeof(longIntegerType));
+printf("Size of long long int: %ld bytes\n",sizeof(LonglongIntegerType));
+printf("Size of float: %ld bytes\n",sizeof(floatType));
+
+return 0;
+}
