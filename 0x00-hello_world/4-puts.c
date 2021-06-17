eaee9bcbc20d353ed77f818973d4269afd15424a
DanielCeron-Holberton <3403@holbertonschool.com>
2021-06-17 22:13:03 +0000

    [FIX] - Fix line to compile

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main(
-char Mystr="\"Programming is like building a multilingual puzzle";
+
+char Mystr = "\"Programming is like building a multilingual puzzle";
+
 puts(Mystr);
 return (0);
 )
