be4663974a8aca837854cff6143ab40bc238458a
DanielCeron-Holberton <3403@holbertonschool.com>
2021-06-17 23:26:09 +0000

    [FIX] - Deleted int on printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * function printf usage
+ * Return: always 0
+*/
+
+int main(void)
+{
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}
