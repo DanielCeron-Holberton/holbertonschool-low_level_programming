439353dde1a78f72dba942b5f792a3f0f9493a73
DanielCeron-Holberton <3403@holbertonschool.com>
2021-06-18 21:03:23 +0000

    [ADD] - 3rd project added

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+int letterLower;
+int letterUpper;
+letterLower = 97;
+letterUpper = 65;
+while (letterLower < 123)
+{
+putchar(letterLower);
+letterLower++;
+}
+while (letterUpper < 90)
+{
+putchar(letterUpper);
+letterUpper++;
+}
+putchar(10);
+return (0);
+}
