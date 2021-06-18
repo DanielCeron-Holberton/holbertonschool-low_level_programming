8ca1ce4687b63712259ba29bad357df33c57fda7
DanielCeron-Holberton <3403@holbertonschool.com>
2021-06-18 00:14:49 +0000

    [FIX] - Typo error fixed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ float floatType;
 long int longIntegerType;
 long long int LonglongIntegerType;
 char charType;
-printf("Size of char: %ld byte\n", sizeof(charType));
-printf("Size of int: %ld bytes\n", sizeof(integerType));
-printf("Size of long int: %ld bytes\n", sizeof(longIntegerType));
-printf("Size of long long int: %ld bytes\n", sizeof(LonglongIntegerType));
-printf("Size of float: %ld bytes\n", sizeof(floatType));
+printf("Size of char: %ld byte(s)\n", sizeof(charType));
+printf("Size of int: %ld byte(s)\n", sizeof(integerType));
+printf("Size of long int: %ld byte(s)\n", sizeof(longIntegerType));
+printf("Size of long long int: %ld byte(s)\n", sizeof(LonglongIntegerType));
+printf("Size of float: %ld byte(s)\n", sizeof(floatType));
 return (0);
 }
