4170d222c7b66d27c3809eae5d3c731c5b4fca04
DanielCeron-Holberton <3403@holbertonschool.com>
2021-06-18 22:27:49 +0000

    [ADD] - Project #6 added

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+int counter = 48;
+while (counter < 58)
+{
+putchar(counter);
+counter++;
+}
+putchar(10);
+return (0);
+}
