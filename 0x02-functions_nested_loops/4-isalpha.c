17156585605ee2ce5321b69dbe2a0dc2a6182a0a
DanielCeron-Holberton <3403@holbertonschool.com>
2021-06-28 18:57:14 +0000

    [FIX] - Check betty code style

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,18 +3,18 @@
  * _isalpha - check uppercase character
  *
  * @c: Input parameter to enter a char
- * 
+ *
  * Return: Should be 1 if is uppercase otherwise should be 0
  */
 
 int _isalpha(int c)
 {
-  if ((c > 64 && c < 91) || (c > 96 && c < 123))
-  {
-  return (1);
-  }
-  else
-  {
-  return (0);
-  }
+if ((c > 64 && c < 91) || (c > 96 && c < 123))
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
