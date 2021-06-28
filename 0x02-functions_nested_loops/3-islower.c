b08b79036ce468c0149d71e0934f7745c77dc809
DanielCeron-Holberton <3403@holbertonschool.com>
2021-06-28 18:33:28 +0000

    [FIX] - Betty fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,14 @@
 #include "holberton.h"
 /**
  * _islower - check lowercase character
- *
+ * @c: parameter for a character
  * Return: Should be 1 if is lowercase otherwise should be 0
  */
 
+
 int _islower(int c)
 {
-if (c > 96 && c < 123 )
+if (c > 96 && c < 123)
 {
 return (1);
 }
