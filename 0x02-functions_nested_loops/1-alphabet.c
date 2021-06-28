7a8c952913e627b94ab929d20386caa27fd34dc7
DanielCeron-Holberton <3403@holbertonschool.com>
2021-06-28 17:49:53 +0000

    [ADD] - New alphabet project added with header and _putchar function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_alphabet - Print all alphabet
+ *
+ * Return: Should be 0
+ */
+void print_alphabet(void)
+{
+int letterLowerCase;
+letterLowerCase = 97;
+while (letterLowerCase < 123)
+{
+_putchar(letterLowerCase);
+letterLowerCase++;
+}
+_putchar(10);
+return;
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -3,5 +3,8 @@
 /*Including libraries*/
 #include <unistd.h>
 #include <stdio.h>
+/* Print char */
 int _putchar(char);
+/* Print all alphabet */
+void print_alphabet(void);
 #endif /* _HOLBERTON_ */
