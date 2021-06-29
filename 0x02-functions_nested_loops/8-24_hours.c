431738da64c3f96e6f0dff13e0345ab8d977f1e8
DanielCeron-Holberton <3403@holbertonschool.com>
2021-06-29 07:00:01 +0000

    [ADD] - 24 hour counter (hours, minutes)

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,16 +1,37 @@
 #include "holberton.h"
 /**
- * print_last_digit - Print the last digit
+ * jack_bauer - Print day (hours,minutes)
  *
  *
- * 
  *
- * Return: the last digit
+ *
  *
  */
 
-void jack_bauer(void){
-
-  
-  
+void jack_bauer(void)
+{
+/*Init vars*/
+int minutesNum;
+int hoursNum;
+minutesNum = 00;
+hoursNum = 00;
+/*Loop to print in bucle*/ 
+while (minutesNum <= 60 && hoursNum < 24)
+{
+if (minutesNum == 60)
+{
+minutesNum = 00;
+hoursNum++;
+}
+if(hoursNum < 24)
+{
+_putchar((hoursNum/10)+'0');
+_putchar((hoursNum%10)+'0');
+_putchar(':');
+_putchar((minutesNum/10)+'0');
+_putchar((minutesNum%10)+'0');
+_putchar(10);
+minutesNum++;
+}
+}
 }
