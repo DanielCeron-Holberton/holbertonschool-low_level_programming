bc27f079430ed5dd080f892a76f12a5f8793339a
DanielCeron-Holberton <3403@holbertonschool.com>
2021-06-29 23:44:36 +0000

    [ADD] - Upper case checker 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _isupper - check uppercase character
+ * @c: parameter for a character
+ * Return: Should be 1 if is uppercase otherwise should be 0
+ */
+
+int _isupper(int c)
+{
+if (c > 64 && c < 91)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,11 @@
+#ifndef _HOLBERTON_
+#define _HOLBERTON_
+/*Including libraries*/
+#include <unistd.h>
+#include <stdio.h>
+/*checks uppercase*/
+int _isupper(int c);
+
+
+
+#endif /* _HOLBERTON_ */
