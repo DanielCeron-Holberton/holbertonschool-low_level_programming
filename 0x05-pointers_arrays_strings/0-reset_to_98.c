adb2fb0ae7db6dfa227ffd2845e947f300bfb2e3
DanielCeron-Holberton <3403@holbertonschool.com>
2021-07-02 00:27:44 +0000

    [ADD] - New project of pointers, readme and 0-reset_to_98.c added

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+  * reset_to_98 - Change the value of n addressed in memory
+  *
+  * @*n: pointer of the variable input
+  *
+  */
+
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,12 @@
+#ifndef _HOLBERTON_
+#define _HOLBERTON_
+
+/*Including libraries*/
+#include <unistd.h>
+#include <stdio.h>
+/*_putchar function*/
+int _putchar(char c);
+/*Updates the value it points to to 98*/
+void reset_to_98(int *n);
+
+#endif /* _HOLBERTON_ */
