65991bdd72ca68f15a893d777a05caaaedcb53e8
DanielCeron-Holberton <3403@holbertonschool.com>
2021-07-08 15:50:08 +0000

    [ADD] - New proyect and task #0 added

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * _strcat - Print characters
+ * @dest: Input string 1
+ * @src: Input string 2
+ * Return: Concatenated strings
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int i;
+char *destPointer = dest;
+char *srcPointer = src;
+int len1 = 0;
+int j;
+for (i = 0; dest[i]; i++)
+{
+len1++;
+destPointer++;
+}
+for (j = 0; src[j]; j++)
+{
+dest[len1] = src[j];
+len1++;
+}
+return(dest);
+}
---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,17 @@
+#ifndef _HOLBERTON_
+#define _HOLBERTON_
+/*Including libraries*/
+#include <stdio.h>
+#include <unistd.h>
+/*_putchar function*/
+int _putchar(char c);
+/*Concatenated Strings*/
+char *_strcat(char *dest, char *src);
+
+
+
+
+
+
+
+#endif /* _HOLBERTON_ */
