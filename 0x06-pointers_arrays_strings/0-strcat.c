e3d1388e67d893ac20c5d176aaeea35d37107f64
DanielCeron-Holberton <3403@holbertonschool.com>
2021-07-08 15:53:01 +0000

    [FIX] - Deleted unused var

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,7 +10,6 @@ char *_strcat(char *dest, char *src)
 {
 int i;
 char *destPointer = dest;
-char *srcPointer = src;
 int len1 = 0;
 int j;
 for (i = 0; dest[i]; i++)
@@ -23,5 +22,5 @@ for (j = 0; src[j]; j++)
 dest[len1] = src[j];
 len1++;
 }
-return(dest);
+return (dest);
 }
