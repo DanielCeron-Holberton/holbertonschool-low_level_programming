311bd927974ff32cd9cd599a647a75a2a0a4f236
root <root@DESKTOP-6ABBR8C.localdomain>
2021-07-13 15:06:05 -0500

    [ADD] - Task #5 added

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * string_toupper - lower to uppercase
+ * Return: Char to uppercase
+ */
+
+
+char *string_toupper(char *str)
+{
+int i = 0;
+
+while (str[i])
+{
+	if (str[i] > 96 && str[i] < 123)
+	{
+	str[i] = str[i] - 32;
+	}
+	i++;
+}
+return (str);
+}
