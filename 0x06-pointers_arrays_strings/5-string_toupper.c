fe2278516297ecd4a645d33240e961bbc7c7d640
root <root@DESKTOP-6ABBR8C.localdomain>
2021-07-13 15:11:19 -0500

    [FIX] - style code use

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,21 +1,20 @@
 #include "holberton.h"
 /**
  * string_toupper - lower to uppercase
+ * @str: Input string
  * Return: Char to uppercase
  */
 
-
 char *string_toupper(char *str)
 {
-int i = 0;
-
-while (str[i])
-{
-	if (str[i] > 96 && str[i] < 123)
+	int i = 0;
+	while (str[i])
 	{
-	str[i] = str[i] - 32;
+		if (str[i] > 96 && str[i] < 123)
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
 	}
-	i++;
-}
-return (str);
+	return (str);
 }
