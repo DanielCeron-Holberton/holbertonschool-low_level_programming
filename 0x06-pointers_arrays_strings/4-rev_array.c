bb5aa98736e7dc184b68a63aed6b8db574bf37bb
root <root@DESKTOP-6ABBR8C.localdomain>
2021-07-13 14:44:05 -0500

    [FIX] - Betty code style

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,20 +7,20 @@
 
 void reverse_array(int *a, int n)
 {
-    int j = 0;
-    int i = n - 1;
-    int k = 0;
-    int newArray[100];
+	int j = 0;
+	int i = n - 1;
+	int k = 0;
+	int newArray[100];
 
-    for (; i >= 0; i--)
-    {
-        newArray[i] = a[j];
-        j++;
-    }
+	for (; i >= 0; i--)
+	{
+		newArray[i] = a[j];
+		j++;
+	}
 
-    while (k < n)
-    {
-        a[k] = newArray[k];
-        k++;
-    }
+	while (k < n)
+	{
+		a[k] = newArray[k];
+		k++;
+	}
 }
