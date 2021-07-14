20702365954ca84230ca4b319f47a04fbd68d6b3
root <root@DESKTOP-6ABBR8C.localdomain>
2021-07-13 19:39:23 -0500

    [ADD] - #6 task added

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * cap_string - Capitalizes words
+ * @str: Input string
+ * Return: Char to uppercase
+ */
+
+char *cap_string(char *str)
+{
+
+	int i;
+	int j;
+	char specialChar[14] = {' ', '\t', '\n', ',', ';', '.', '!', '?',
+	'\"', '(', ')', '{', '}', '\0'};
+
+	/*Go through input array*/
+	for (; str[i]; i++)
+	{
+
+		for (j = 0; specialChar[j]; j++)
+		{
+			if (str[i] > 96 && str[i] < 123)
+			{
+				if (str[i - 1] == specialChar[j])
+				{
+					str[i] = str[i] - 32;
+				}
+			}
+		}
+	}
+	return (str);
+}
