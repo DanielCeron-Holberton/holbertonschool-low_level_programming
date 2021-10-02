607edfcf896952561874e3336e5fe16a2169cdec
DanielCeron-Holberton <3403@holbertonschool.com>
2021-10-01 19:35:24 -0500

    [ADD] - 12th task added

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+ * main - Prints combinations of two pairs of digits
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+
+int i;
+int j;
+int k;
+int l;
+
+for (i = '0'; i <= '9'; i++)
+{
+for (j = '0'; j <= '9'; j++)
+{
+for (k = '0'; k <= '9'; k++)
+{
+for (l = '1'; l <= '9'; l++)
+{
+if ((i != j) | (i == j && i == '0'))
+{
+putchar(i);
+putchar(j);
+putchar(' ');
+putchar(k);
+putchar(l);
+if (i == '9' && j == '8' && k == '9' && l == '9')
+{
+putchar(10);
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+}
+return (0);
+}
