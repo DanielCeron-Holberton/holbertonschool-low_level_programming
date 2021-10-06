05707124ed6f93e9d29b72bdf96e0d0978a66163
DanielCeron-Holberton <3403@holbertonschool.com>
2021-10-06 12:53:16 -0500

    [FIX] - libraries

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,8 +2,11 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 void positive_or_negative(int i);
+/*Print largest number*/
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
