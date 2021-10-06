8dc30b05674d217d753f0a7c27d3356fa97da71b
DanielCeron-Holberton <3403@holbertonschool.com>
2021-10-06 02:41:14 -0500

    [FIX] - betty and output fix

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,25 +8,24 @@
 
 int main(void)
 {
-	int f1 = 0;
-	int f2 = 1;
-	int i = 0;
-	int next = 0;
+	long int f1 = 1;
+	long int f2 = 1;
+	long int i = 0;
+	long int next = 0;
 
 	for (i = 1; i < 50; i++)
 	{
 		if (i < 49)
 		{
-			printf("%d, ", f2);
+			printf("%ld, ", f2);
 		}
 		else
 		{
-			printf("%d", f2);
+			printf("%ld", f2);
 		}
 		next = f1 + f2;
 		f1 = f2;
 		f2 = next;
-		
 	}
 	return (0);
 }
