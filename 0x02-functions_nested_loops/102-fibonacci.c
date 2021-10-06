0727dd91036a3bbb7502a8624253b50b17be11d5
DanielCeron-Holberton <3403@holbertonschool.com>
2021-10-05 23:07:32 -0500

    [ADD] - Task 14th added

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - prints 50 numbers of fibonacci sequence
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int f1 = 0;
+	int f2 = 1;
+	int i = 0;
+	int next = 0;
+
+	for (i = 1; i < 50; i++)
+	{
+		if (i < 49)
+		{
+			printf("%d, ", f2);
+		}
+		else
+		{
+			printf("%d", f2);
+		}
+		next = f1 + f2;
+		f1 = f2;
+		f2 = next;
+		return (0);
+	}
