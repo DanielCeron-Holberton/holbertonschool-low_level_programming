538dca70cd5be14176c57e0b0767eb56945e228b
DanielCeron-Holberton <3403@holbertonschool.com>
2021-10-11 23:12:35 -0500

    [FIX] - Hotfix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,17 +9,18 @@
 
 void print_array(int *a, int n)
 {
-int i;
-for (i = 0; i < n; i++)
-{
-if (i == n - 1)
-{
-printf("%d", a[i]);
-}
-else
-{
-printf("%d, ", a[i]);
-}
-}
-_putchar(10);
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+		}
+	}
+	_putchar(10);
 }
