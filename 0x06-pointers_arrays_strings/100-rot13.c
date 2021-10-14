e5e2a56712635281e669ba9de6967ea4f7cd6d4b
DanielCeron-Holberton <3403@holbertonschool.com>
2021-10-14 17:21:48 -0500

    [ADD] - 8th task added/ rot encoder

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * rot13 - converts strings in to rot13 cipher
+ *
+ * @str: Input string
+ * Return: output string
+ */
+
+
+
+char *rot13(char *str)
+{
+	char a[] = {"ABCDEFGHIJKLMabcdefghijklm"};
+
+	char b[] = {"NOPQRSTUVWXYZnopqrstuvwxyz"};
+
+	int i = 0, j = 0;
+
+	for (; str[i]; i++)
+	{
+		for (j = 0; a[j]; j++)
+		{
+			if (str[i] == a[j])
+			{
+				str[i] = b[j];
+			}
+			else if (str[i] == b[j])
+			{
+				str[i] = a[j];
+			}
+		}
+	}
+}
