36a303e0ca6c6c6036e7b2a09dd5bac33d49ddbd
Daniel-Ceron <3403@holbertonschool.com>
2021-10-18 18:29:45 -0500

    [FIX] - Function prototype added to main.h and int var changed to unsinged int

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,6 +7,8 @@
 char *_memset(char *s, char b, unsigned int n);
 /*Print buffer function proto*/
 void simple_print_buffer(char *buffer, unsigned int size);
+/*Memcpy function proto*/
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 
 #endif /* MAIN_HEADER */
