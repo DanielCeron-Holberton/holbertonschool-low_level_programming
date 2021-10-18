e166ccf2526d1487901cfffe0fa6e0efe8c14434
Daniel-Ceron <3403@holbertonschool.com>
2021-10-18 18:01:42 -0500

    [FIX] - adding header instruction and description of the function

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,3 +1,17 @@
+#include "main.h"
+
+
+/**
+ * _memset - fills n bytes of s
+ * @s: entry array/string
+ * @b: constant byte
+ * @n: number of bytes
+ *
+ * Return: String/array returned after change
+ */
+
+
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
