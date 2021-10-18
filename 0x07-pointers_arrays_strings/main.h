55a6aecb85646a8ebc6a1dd1b0390b9464d42a8d
Daniel-Ceron <3403@holbertonschool.com>
2021-10-18 17:40:19 -0500

    [FIX] - Prototype added

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,5 +4,8 @@
 
 /*Memset Function proto*/
 char _memset(char *s, char b, unsigned int n);
+/*Print buffer function proto*/
+void simple_print_buffer(char *buffer, unsigned int size);
+
 
 #endif /* MAIN_HEADER */
