bb366cfa329f68018d8df61e8202d410c3bb77d1
Daniel-Ceron <3403@holbertonschool.com>
2021-10-18 18:25:28 -0500

    [ ADD] - memcpy function task added

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy n bytes of source
+ * @dest: destiny array/string
+ * @src: entry bytes/array/string
+ * @n: number of bytes
+ *
+ * Return: String/array returned after change
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+	int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
