dc7fd116c0cc6d11c4e0947a24051077bce1c961
Daniel-Ceron <3403@holbertonschool.com>
2021-10-18 19:13:51 -0500

    [ FIX] Betty style code fixed

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strchr - Returns pointer on first ocurrence 
+ * _strchr - Returns pointer on first ocurrence
  * @s: source string
  * @c: constant character to compare
  * Return: return pointer to the first occurrence
@@ -9,17 +9,17 @@
 
 char *_strchr(char *s, char c)
 {
-    int i = 0;
-    char *p;
+	int i = 0;
+	char *p;
 
-    while (s[i])
-    {
-        if (s[i] == c)
-        {
-            p = &s[i];
-            return (p);
-        }
-        i++;
-    }
-    return (0);
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			p = &s[i];
+			return (p);
+		}
+		i++;
+	}
+	return (0);
 }
