d2a20119a978c365f4dbf7e0de8da6f2dcb350e7
Daniel-Ceron <3403@holbertonschool.com>
2021-10-19 20:48:35 -0500

    [FIX] - betty style code

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -4,21 +4,21 @@
  * print_diagsums - print the diag sums of a matrix
  * @a: matrix input
  * @size: size of the matrix
- * 
+ *
  */
 
 void print_diagsums(int *a, int size)
 {
-    int i, sum = 0, sum1 = 0;
+	int i, sum = 0, sum1 = 0;
 
-    for (i = 0; i < size; i++)
-    {
-        sum += a[i * size + i];
-    }
-    for (i = 1;  i <= size; i++)
-    {
-        sum1 += a[i * size - i];
-    }
+	for (i = 0; i < size; i++)
+	{
+		sum += a[i * size + i];
+	}
+	for (i = 1; i <= size; i++)
+	{
+		sum1 += a[i * size - i];
+	}
 
-    printf("%d, %d\n", sum, sum1);
+	printf("%d, %d\n", sum, sum1);
 }
