b318ec52e8bf285c059eaa2f6e38e0c280b139ef
Daniel-Ceron <3403@holbertonschool.com>
2021-10-20 23:15:56 -0500

    [FIX]- Hot fix

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - Reverses a string with recursion 
- * 
+ * _print_rev_recursion - Reverses a string with recursion
+ *
  * @s: entry string
  *
  */
@@ -12,6 +12,6 @@ void _print_rev_recursion(char *s)
 	if (*s)
 	{
 		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		putchar(*s);
 	}
 }
