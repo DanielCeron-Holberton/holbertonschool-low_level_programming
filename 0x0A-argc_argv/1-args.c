f5b1bea821fac329adc7bacfedcc01c49fa9fbe9
DanielCeron-Holberton <3403@holbertonschool.com>
2021-10-22 13:02:19 -0500

    [ADD] - Task #1 that counts arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - prints the number of arguments passed into it.
+ *
+ * @argc: count the arguments passed
+ * @argv: Array of the address values
+ * Return: Zero
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	printf("%d\n", argc);
+	for (; argv[i]; i++)
+	{
+		i++;
+	}
+
+
+	return (0);
+}
