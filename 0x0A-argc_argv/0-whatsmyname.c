0c1f1df60fd0a70c3076cb0fa4f5d930f350b759
DanielCeron-Holberton <3403@holbertonschool.com>
2021-10-22 12:47:45 -0500

    [ADD] - New proyect added 0x0A ARGC, ARGV

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print the name of the program
+ *
+ *
+ * @argc: Count arguments in command line
+ * @argv: Array with the address value of the arguments
+ * Return: Zero
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for(; i < argc; i++);
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+#include <stdio.h>
+
+
+
+
+
+
+
+#endif /* MAIN_HEADER */
