437be3b6393dc09e6773d6a6972088a2fbe8ec3d
DanielCeron-Holberton <3403@holbertonschool.com>
2021-10-26 11:48:02 -0500

    [FIX] - Return statement Malloc Issue

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,13 +10,12 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
-	char *newArray = malloc(size * sizeof(char));
+	char *newArray = (char *)malloc(size * sizeof(char));
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
-
 	for (i = 0; i < size; i++)
 	{
 		newArray[i] = c;
