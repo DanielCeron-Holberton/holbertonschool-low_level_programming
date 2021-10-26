df55fd485b27d396365695d84d26a3bf471b522f
DanielCeron-Holberton <3403@holbertonschool.com>
2021-10-26 12:23:00 -0500

    [ADD] - Task #1 added

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ *
+ * @str: input string
+ *
+ * Return: Returns null if string is not allocated or returns string allocated
+ * in different mem location
+ *
+ */
+
+char *_strdup(char *str)
+{
+	int size = 0;
+	int i = 0;
+
+	char *newString;
+
+	for (size = 0; str[size]; size++)
+		newString = (char *)malloc((size + 1) * sizeof(char));
+	if (newString == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			newString[i] = str[i];
+		}
+	}
+
+	return (newString);
+}
