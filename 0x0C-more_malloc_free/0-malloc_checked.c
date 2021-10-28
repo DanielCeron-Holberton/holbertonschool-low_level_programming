803769354fafcbf2722ef9916fae97a235b90a21
DanielCeron-Holberton <3403@holbertonschool.com>
2021-10-28 16:59:23 -0500

    [FIX] - New line added and blankspaces deleted

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
 /**
- * malloc_checked - allocates and checks the input data 
- * 
- * @b: bytes to allocate 
- *  
+ * malloc_checked - allocates and checks the input data
+ *
+ * @b: bytes to allocate
+ *
  */
 
 void *malloc_checked(unsigned int b)
 {
-	void *allockedInt = (void *)malloc(b);
+	void *allockedInt;
+
+	allockedInt = malloc(b);
+
 	if (allockedInt == NULL)
 	{
 		exit(98);
