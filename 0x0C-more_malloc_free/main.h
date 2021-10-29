e61f15596dc7532af33a697ee5c27189be2a7159
DanielCeron-Holberton <3403@holbertonschool.com>
2021-10-28 22:31:51 -0500

    [ADD] - Task #2 and his proto

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,9 +5,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <string.h>
 
-
+void simple_print_buffer(char *buffer, unsigned int size);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif /* MAIN_HEADER */
