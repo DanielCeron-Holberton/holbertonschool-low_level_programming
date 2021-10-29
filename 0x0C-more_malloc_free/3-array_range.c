b618ce2d50ea00a3a9b8759161139fd3c95786cc
DanielCeron-Holberton <3403@holbertonschool.com>
2021-10-29 00:00:33 -0500

    [ADD] - new task

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * array_range - new array
+ * 
+ * @min:min
+ * @max:max
+ * Return: INT
+ */
+
+int *array_range(int min, int max)
+{
+	int *poin;
+	int total, i;
+
+	if (min > max)
+		return (NULL);
+	total = max - min + 1;
+	poin = malloc(sizeof(int) * total);
+	if (poin == NULL)
+		return (NULL);
+	for (i = 0; min <= max; i++)
+	{
+		*(poin + i) = min;
+		min++;
+	}
+	return (poin);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,5 +10,6 @@ void simple_print_buffer(char *buffer, unsigned int size);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 
 #endif /* MAIN_HEADER */
