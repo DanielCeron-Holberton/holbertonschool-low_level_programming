dc927c0c3adc28a22ac636206b1f041a0af24996
DanielCeron-Holberton <3403@holbertonschool.com>
2021-11-01 18:15:17 -0500

    [ADD] - Task #1 / Init a Structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+ * init_dog - Inits a dog structure
+ *
+ * @d: entry structure
+ * @name: 1st data
+ * @age: 2nd data
+ * @owner:3rd data
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	/* (*d).name = name;*/
+	/* (*d).age = age;*/
+	/* (*d).owner = owner;*/
+
+}
