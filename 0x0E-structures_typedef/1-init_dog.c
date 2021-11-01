152345ac8368ab0ebf0bfaed3702b0f0239f19ab
DanielCeron-Holberton <3403@holbertonschool.com>
2021-11-01 18:29:35 -0500

    [FIX] - Hotfix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - Inits a dog structure
@@ -10,13 +11,15 @@
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-
+{	
+	if (d == NULL)
+	{
+		return (NULL);
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
 	/* (*d).name = name;*/
 	/* (*d).age = age;*/
 	/* (*d).owner = owner;*/
-
 }
