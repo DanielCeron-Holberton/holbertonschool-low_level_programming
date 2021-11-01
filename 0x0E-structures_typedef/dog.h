8809f0f069fa8b4b30c911fcb67cccc7a45bec90
DanielCeron-Holberton <3403@holbertonschool.com>
2021-11-01 17:46:15 -0500

    [ADD] - New Project, README and Task #0 dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_STRUCTURE_
+#define _DOG_STRUCTURE_
+
+/**
+ * struct dog - dog structure
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner's name
+ * Description: Creates a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*_DOG_STRUCTURE_*/
