0979194d9b6c976204f5bae8497e0c1b7c77b42c
DanielCeron-Holberton <3403@holbertonschool.com>
2021-11-01 12:44:13 -0500

    [ADD] - Task #2 added

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the origin name c file of
+ * the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s", __BASE_FILE__);
+	return (0);
+}
