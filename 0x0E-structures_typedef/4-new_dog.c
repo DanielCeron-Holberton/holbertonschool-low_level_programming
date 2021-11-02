7b145d149cc49f91c3bd3aa42d70efb1b982b52a
DanielCeron-Holberton <3403@holbertonschool.com>
2021-11-01 23:16:57 -0500

    [FIX] - hotfix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,6 +13,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *create_new_dog;
+	char *ptr1, *ptr2;
 
 	create_new_dog = malloc(sizeof(dog_t));
 	if (create_new_dog == NULL)
@@ -21,26 +22,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	if (name != NULL)
 	{
-		create_new_dog->name = malloc(sizeof(char) * strlen(name));
-		if (create_new_dog->name == NULL)
+		ptr1 = malloc(sizeof(char) * strlen(name));
+		if (ptr1 == NULL)
 		{
 			free(create_new_dog);
 			return (NULL);
 		}
-		create_new_dog->name = name;
+		create_new_dog->name = strcpy(ptr1, name);
 	}
 	else
 		create_new_dog->name = NULL;
 	if (owner != NULL)
 	{
-		create_new_dog->owner = malloc(sizeof(char) * strlen(owner));
-		if (create_new_dog->owner == NULL)
+		ptr2 = malloc(sizeof(char) * strlen(owner));
+		if (ptr2 == NULL)
 		{
-			free(create_new_dog->name);
+			free(ptr1);
 			free(create_new_dog);
 			return (NULL);
 		}
-		create_new_dog->owner = owner;
+		create_new_dog->owner = strcpy(ptr2, owner);
 	}
 	else
 	{
