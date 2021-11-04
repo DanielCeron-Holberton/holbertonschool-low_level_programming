90179820543926b0057d241291580bd1c1d8c606
DanielCeron-Holberton <3403@holbertonschool.com>
2021-11-03 22:12:12 -0500

    [FIX] - conditionals to error fixed

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,29 +12,29 @@ int main(int argc, char *argv[])
 	int i = 0;
 	int j = 0;
 	char *p = argv[2];
-	int result = 0;
-	/*int argc_copy = argc;*/
-
-	if (argc > 4 || argc <= 3)
+	int (*result)(int, int);
+	
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	if ( strlen(p) > 1)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	i = atoi(argv[1]);
+	j = atoi(argv[3]);
 
-	if ((*p != '+' && *p != '-' && *p != '*' && *p != '/' &&
-	     *p != '%') ||
-	    strlen(p) > 1)
+	result = get_op_func(p);
+	if (result == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	i = atoi(argv[1]);
-	j = atoi(argv[3]);
-
-	result = get_op_func(p)(i, j);
-
-	printf("%d \n", result);
+	printf("%d \n", result(i, j));
 
 	return (0);
 }
