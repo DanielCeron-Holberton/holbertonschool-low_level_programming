f83bef36c1eb5a35ca9def7ab5ecdb8e3fb9b28a
DanielCeron-Holberton <3403@holbertonschool.com>
2021-11-04 19:50:27 -0500

    [FIX] - type of data fixed to unsigned

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,20 +10,20 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list ap;
 	unsigned int i = 0;
-
+	va_list ap;
+	
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
 		if (i == n - 1 || separator == NULL)
 		{
-			printf("%d\n", va_arg(ap, int));
+			printf("%d\n", va_arg(ap, unsigned int));
 		}
 		else
 		{
-			printf("%d%s", va_arg(ap, int), separator);
+			printf("%d%s", va_arg(ap, unsigned int), separator);
 		}
 		}
 	va_end(ap);
