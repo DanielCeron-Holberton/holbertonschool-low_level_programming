0c5a99f70a0f097df15771417a924a15228bc115
DanielCeron-Holberton <3403@holbertonschool.com>
2021-11-04 23:44:26 -0500

    [ADD] - Task #3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - Prints all type of data
+ *
+ * @format: format to print
+ *
+ */
+void print_all(const char *const format, ...)
+{
+	va_list ap;
+	char *sval, *p, *separator = ", ";
+	char cval;
+	float fval;
+	int ival;
+	int i = 0;
+
+	va_start(ap, format);
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+		case 'c':
+			cval = va_arg(ap, int);
+			printf("%c", cval);
+			break;
+		case 'f':
+			fval = va_arg(ap, double);
+			printf("%f", fval);
+			break;
+		case 'i':
+			ival = va_arg(ap, int);
+			printf("%d", ival);
+			break;
+		case 's':
+			sval = va_arg(ap, char *);
+			if (!sval)
+				printf("(nil)");
+			printf("%s", sval);
+			break;
+		default:
+			i++;
+			continue;
+		}
+		printf("%s", separator);
+		p++;
+	}
+	printf("\n");
+	va_end(ap);
+}
