d77e36e887c0cdf492c06fedd835a70897a135a8
DanielCeron-Holberton <3403@holbertonschool.com>
2021-11-14 00:43:46 -0500

    [ADD] - Task #4 - Frees a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_list - frees a linked list previosly
+ * allocated in heap 
+ * 
+ * @head: entry head of the list
+ */
+
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = temp->next;
+		free(temp->str);
+		free(temp);
+	}
+}
