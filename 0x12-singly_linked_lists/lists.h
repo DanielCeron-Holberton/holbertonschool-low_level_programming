df8cde8dc407b6f151aa928a91bba6025d3ca2fd
DanielCeron-Holberton <3403@holbertonschool.com>
2021-11-13 23:19:03 -0500

    [FIX] - spaces changed for identation

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -15,13 +14,11 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
-
-
 
 #endif /*LIST_HEADER*/
