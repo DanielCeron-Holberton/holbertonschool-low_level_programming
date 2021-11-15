559f6387e4aa506f5fe46bc2ef95abfae3d3aa3f
DanielCeron-Holberton <3403@holbertonschool.com>
2021-11-15 12:05:39 -0500

    [FIX] - Identation fixed

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,6 @@
 #ifndef HEADER_LISTS
 #define HEADER_LISTS
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,17 +11,14 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ *
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
-
 size_t print_listint(const listint_t *h);
-
-
 
 #endif /*HEADER_LISTS*/
