8607b4e920aa321a96e1c6eb9384918bb43ac67d
DanielCeron-Holberton <3403@holbertonschool.com>
2021-11-16 11:54:22 -0500

    [FIX] - Conditional for null fixed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	temp = *head;
-	while (i < idx - 1 && temp != NULL)
+	while (i < idx - 1 && *head != NULL)
 	{
 		temp = temp->next;
 		i++;
