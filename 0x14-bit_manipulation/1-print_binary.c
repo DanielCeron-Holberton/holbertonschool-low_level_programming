19f9458bb3af6d07a1787da31ab78aa7491cb762
Daniel Ceron <3403@holbertonschool.com>
2021-11-18 18:58:46 -0500

    [ADD] - Task #1 added

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_binary - print a decimal number in binary 
+ * 
+ * @n: Decimal number
+ */
+
+void print_binary(unsigned long int n)
+{
+
+	unsigned long int temp = n;
+	int size = 0;
+
+	while (temp > 0)
+	{
+		temp >>= 1;
+		size++;
+	}
+	size--;
+
+	if (size < 0)
+		putchar('0');
+
+	while (size >= 0)
+	{
+		if ((n >> size) & 1)
+			putchar('1');
+		else
+			putchar('0');
+		size--;
+	}
+}
