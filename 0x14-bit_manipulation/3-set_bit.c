bca823ba7ec48be2e7a87e342c0f6c59afbcf8c7
Daniel Ceron <3403@holbertonschool.com>
2021-11-18 20:37:26 -0500

    [ADD] - Task #3 added

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * set_bit - Set the bit object
+ *
+ * @n: entry number
+ * @index: index of the number
+ * Return: int 
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > (sizeof(n) * 8))
+		return (-1);
+	*n |= 1 << index;
+	return (1);
+}
