185486ef8da99bd552ed2b3b848004e8f6ebe34b
DanielCeron-Holberton <3403@holbertonschool.com>
2022-01-06 23:18:38 -0500

    [ADD] - New tasks added

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - Get the dnodeint at index object
+ *
+ * @head: head of the link
+ * @index: position
+ * Return: address of the new node 
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+
+	while (i < index)
+	{
+		head = head->next;
+		if (head == NULL)
+			return (NULL);
+		i++;
+	}
+
+	return (head);
+}
