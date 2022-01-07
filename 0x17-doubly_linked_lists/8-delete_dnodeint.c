b832bee585d1622dbae534e49bf6e70438b141f8
Daniel Ceron <85509317+DanielCeron-Holberton@users.noreply.github.com>
2022-01-07 14:36:04 -0500

    [FIX] - Conditional and index initial value fixed

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,9 +10,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 
 	dlistint_t *current;
-	unsigned int i = 1;
+	unsigned int i = 0;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (-1);
 
 	current = *head;
