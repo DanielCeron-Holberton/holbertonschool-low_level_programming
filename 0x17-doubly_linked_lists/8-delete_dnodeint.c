04e0d3d532059a507f54e7719563ac543aa0f383
DanielCeron-Holberton <3403@holbertonschool.com>
2022-01-07 00:02:56 -0500

    [FIX] - Hotfix

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,7 +10,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 
 	dlistint_t *current;
-	unsigned int i = 0;
+	unsigned int i = 1;
 
 	if (*head == NULL)
 		return (-1);
@@ -19,9 +19,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	while (i < index)
 	{
-		current = current->next;
+
 		if (current == NULL)
 			return (-1);
+		current = current->next;
+
 		i++;
 	}
 	if (current == *head)
@@ -29,6 +31,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (current->next != NULL)
 		current->next->prev = current->prev;
+
+	if (current->prev != NULL)
+		current->prev->next = current->next;
+
+	free(current);
 
 	return (1);
 }
