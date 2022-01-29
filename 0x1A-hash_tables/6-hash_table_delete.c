744039756ff8ce7f3df4189cbf888357c19bf4dd
Daniel Ceron <danielceron@pop-os.localdomain>
2022-01-28 20:47:58 -0500

    [FIX] - HotFix

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,6 +1,21 @@
 #include "hash_tables.h"
 
 
+
+void free_hash_list(hash_node_t *node)
+{
+	
+	if (!node)
+		return;
+	free_hash_list(node->next);
+	free(node->key);
+	free(node->value);
+	free(node);
+}
+
+
+
+
 /**
  * hash_table_delete - Deletes a Hash table 
  * 
@@ -9,17 +24,13 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *temp;
 
-	for (i = 0; i <= ht->size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
-		while (ht->array[i]->next)
-		{
-			temp = ht->array[i];
-			ht->array[i] = ht->array[i]->next;
-			free(temp);
-		}
-		free(ht->array[i]);
+		free_hash_list(ht->array[i]);
 	}
+	free(ht->array);
 	free(ht);
 }
+
+
